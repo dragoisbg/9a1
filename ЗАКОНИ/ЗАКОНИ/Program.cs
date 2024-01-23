739bf82fd815f66d71b5c182438c803aa3523bee
dragoisbg <157458788+dragoisbg@users.noreply.github.com>
2024-01-23 10:13:56 +0200

    Add files via upload

---
+++ ЗАКОНИ/ЗАКОНИ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЗАКОНИ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool a = false;
+            
+            bool b = false;
+            Console.WriteLine((a || b) + "e 0");
+            Console.WriteLine(" 0 i 1  ="+( a || !b)+ " e 1");
+            Console.WriteLine((!a || b) + " e 1");
+            Console.WriteLine((a || b) + " e 0");
+           
+
+
+
+
+        }
+    }
+}
